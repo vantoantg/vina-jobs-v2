04773782defc0c3216bea50db1a61e97c7a78426
tona-lc <nvtoan@lockon-vn.com>
2018-02-12 12:44:30 +0700

    dev

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=vinajobs_db',
     'username' => 'root',
-    'password' => 'tonaroot',
+    'password' => '',
     'charset' => 'utf8',
 
 //    Schema cache options (for production environment)
