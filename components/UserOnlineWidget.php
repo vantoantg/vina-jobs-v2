bcf40d750e039487840b6cfe4c376b61ddf4be90
Mr.Tona <nguyennguyen.vt88@gmail.com>
2018-02-27 23:29:41 +0700

    Add helper

---
+++ components/UserOnlineWidget.php
@@ -0,0 +1,146 @@
+<?php
+namespace app\components;
+use app\components\tona\Cons;
+use app\library\helper\Helper;
+use app\library\helper\Datetime;
+use app\models\System;
+use app\models\UserOnline;
+use Carbon\Carbon;
+use yii\base\Widget;
+use Yii;
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 6/12/2016
+ * Time: 8:20 PM
+ */
+class UserOnlineWidget extends Widget
+{
+	public $message;
+	public static $statistics = [];
+	public static $total = 0;
+	public function init()
+	{
+		parent::init();
+	}
+
+	public function run()
+	{
+	}
+
+	public static function userOnline(){
+		if (!isset($_SESSION)) {
+			session_start();
+		}
+		$session = session_id();
+		if (self::check($session) == false) {
+			$u = new UserOnline();
+			$u->session = $session;
+			$u->time = Carbon::now()->format(Cons::SQL_DATE_TIME);
+			$u->browser = Helper::getBrowser();
+			$u->url = Helper::getCurrentUrl();
+			$u->ip = Helper::getIpClient();
+			$u->controller = Yii::$app->controller->id;
+			$u->action = Yii::$app->controller->action->id;
+			$u->method = Helper::getMethod();
+			$u->save();
+		}
+
+	}
+
+	/**
+	 * @return \stdClass
+	 */
+	public static function getStatistic(){
+		self::userOnline();
+
+		if(!self::$statistics){
+			self::$statistics = self::countUserOnline();
+		}
+
+		return Helper::arrayToObject(self::$statistics);
+	}
+
+	/**
+	 * @param $session
+	 * @return bool
+	 */
+	public static function check($session){
+		$time = Carbon::now()->timestamp - 600;
+		$timeCheck = Carbon::createFromTimestamp($time)->format(Datetime::SQL_DATE_TIME);
+		$uo = UserOnline::find()->where(['session' => $session])->andWhere("time > '$timeCheck'")->one();
+		if($uo){
+			$uo->time = Carbon::now()->format(Datetime::SQL_DATE_TIME);
+			$uo->save();
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @return array
+	 */
+	public static function countUserOnline(){
+		$lastWeek = Carbon::now()->subDay(7)->timestamp;
+		$lastMonth = Carbon::now()->subDay(30)->timestamp;
+		$today = Carbon::createFromFormat(Datetime::SQL_DATE_TIME, date('Y-m-d'). ' 00:00:00')->timestamp;
+
+		$week = 0;
+		$month = 0;
+		$day = 0;
+
+		$us = UserOnline::find()->all();
+
+		if($us){
+			foreach($us as $value){
+				$time = Carbon::createFromFormat(Datetime::SQL_DATE_TIME, $value->time)->timestamp;
+				if($time > $lastWeek){
+					$week += 1;
+				}
+				if($time > $lastMonth){
+					$month += 1;
+				}
+				if($time > $today){
+					$day +=1;
+				}
+			}
+		}
+		self::$total = $month + System::find()->where(['code' => 'user_online_total'])->count();
+		return [
+			'today' => $day,
+			'week' 	=> $week,
+			'month' => $month,
+			'total' => self::$total
+		];
+	}
+
+	public static function test(){
+
+		for($i=1; $i < 700000; $i++) {
+			$u = new UserOnline();
+			$u->session = md5(rand(111,999));
+			$u->time = Carbon::now()->format(Cons::SQL_DATE_TIME);
+			$u->browser = Helper::getBrowser();
+			$u->url = Helper::getCurrentUrl();
+			$u->ip = Helper::getIpClient();
+			$u->controller = Yii::$app->controller->id;
+			$u->action = Yii::$app->controller->action->id;
+			$u->method = Helper::getMethod();
+			$u->save();
+		}
+	}
+
+	public static function clearUserOnline(){
+		$total = count(UserOnline::find()->all()) - self::getStatistic()->month;
+		$s = System::find()->where(['code' => 'user_online_total'])->one();
+		$s->value_number = $s->value_number + $total;
+		$s->save();
+
+		$lastMonth = Carbon::now()->subDay(30)->format(Cons::SQL_DATE_TIME);
+		$sql = "DELETE FROM tn_user_online WHERE time < '$lastMonth'";
+		Yii::$app->db->createCommand($sql)->query();
+
+
+	}
+}
--- library/helper/Helper.php
+++ library/helper/Helper.php
@@ -90,10 +90,277 @@ class Helper
         return $routes;
     }
 
+	/**
+	 * @return mixed
+	 */
+	public static function getBrowser(){
+		return  $_SERVER['HTTP_USER_AGENT'];
+	}
+
 	public static function test(){
         echo '<pre>';
         print_r(Yii::$app->controller->getRoute());
         echo '</pre>';
         die;
     }
+
+	/**
+	 * @return mixed
+	 */
+	public static function getMethod(){
+		return $_SERVER['REQUEST_METHOD'];
+	}
+	/**
+	 * @return mixed
+	 */
+	public static function getIpClient(){
+		return $_SERVER['SERVER_ADDR'];
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function getCurrentUrl(){
+		return "http" . (($_SERVER['SERVER_PORT'] == 443) ? "s://" : "://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	}
+	/**
+	 * @param $name
+	 * @return mixed
+	 */
+	public static function getCookie($name){
+		$return = explode('~',$name);
+		if(!isset($_COOKIE[$name])) {
+			return $return[0];
+		} else {
+			return $return[1];
+		}
+	}
+
+	/**
+	 * @param $array
+	 * @return \stdClass
+	 */
+	public static function arrayToObject($array) {
+		$object = new \stdClass();
+		foreach ($array as $key => $value) {
+			if (is_array($value)) {
+				$value = self::arrayToObject($value);
+			}
+			$object->$key = $value;
+		}
+		return $object;
+	}
+
+	/**
+	 * @param $object
+	 * @return array
+	 */
+	public static function objectToArray($object){
+		$arrays = [];
+		foreach($object as $key => $value){
+			if (is_object($value)) {
+				$value = self::objectToArray($value);
+			}
+			$arrays[$key] = $value;
+		}
+		return $arrays;
+	}
+
+	/**
+	 * @param $strUrl
+	 * @return string
+	 */
+	public function checkUrlHttp($strUrl){
+		$arrParsedUrl = parse_url($strUrl);
+		if (!empty($arrParsedUrl['scheme']))
+		{
+			// Contains http:// schema
+			if ($arrParsedUrl['scheme'] === "http"){
+			}
+			// Contains https:// schema
+			else if ($arrParsedUrl['scheme'] === "https"){
+
+			}
+			return $strUrl;
+		}else{
+			return 'http://'.$strUrl;
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function urlTemplate(){
+		return self::siteURL().'/web/template/'.Cons::TEMPLATE_FOLDER.'/';
+	}
+
+	/**
+	 * Function used to create a slug associated to an "ugly" string.
+	 *
+	 * @param string $string the string to transform.
+	 *
+	 * @return string the resulting slug.
+	 */
+	public static function createSlug($strings) {
+		$string = self::stripUnicode($strings);
+		$table = array(
+			'�'=>'S', '�'=>'s', '?'=>'Dj', '?'=>'dj', '�'=>'Z', '�'=>'z', '?'=>'C', '?'=>'c', '?'=>'C', '?'=>'c',
+			'�'=>'A', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'A', '�'=>'C', '�'=>'E', '�'=>'E',
+			'�'=>'E', '�'=>'E', '�'=>'I', '�'=>'I', '�'=>'I', '�'=>'I', '�'=>'N', '�'=>'O', '�'=>'O', '�'=>'O',
+			'�'=>'O', '�'=>'O', '�'=>'O', '�'=>'U', '�'=>'U', '�'=>'U', '�'=>'U', '�'=>'Y', '�'=>'B', '�'=>'Ss',
+			'�'=>'a', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'a', '�'=>'c', '�'=>'e', '�'=>'e',
+			'�'=>'e', '�'=>'e', '�'=>'i', '�'=>'i', '�'=>'i', '�'=>'i', '�'=>'o', '�'=>'n', '�'=>'o', '�'=>'o',
+			'�'=>'o', '�'=>'o', '�'=>'o', '�'=>'o', '�'=>'u', '�'=>'u', '�'=>'u', '�'=>'y', '�'=>'y', '�'=>'b',
+			'�'=>'y', '?'=>'R', '?'=>'r', '/' => '-', ' ' => '-', '.' => '', ',' => '', ';' => '', ':' => '-', '@' => '-'
+		);
+
+		// -- Remove duplicated spaces
+		$stripped = preg_replace(array('/\s{2,}/', '/[\t\n]/'), ' ', $string);
+
+		// -- Returns the slug
+		return strtolower(strtr($string, $table));
+	}
+
+	/**
+	 * @param $str
+	 * @return mixed
+	 */
+	public static function stripUnicode($str)
+	{
+		$str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+		$str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+		$str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+		$str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+		$str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+		$str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+		$str = preg_replace("/(đ)/", 'd', $str);
+		$str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+		$str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+		$str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+		$str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+		$str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+		$str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+		$str = preg_replace("/(Đ)/", 'D', $str);
+		$str = str_replace("  ", " ", $str);
+		$str = str_replace("   ", " ", $str);
+		$str = str_replace("    ", " ", $str);
+		$str = str_replace("     ", " ", $str);
+		$str = str_replace("      ", " ", $str);
+		$str = str_replace("       ", " ", $str);
+		$str = str_replace("        ", " ", $str);
+		$str = str_replace("         ", " ", $str);
+		$str = str_replace("          ", " ", $str);
+		$str = str_replace("           ", " ", $str);
+		return $str;
+	}
+
+	/**
+	 * @param $str
+	 * @param int $words
+	 * @return string
+	 */
+	public static function cutStringSpace($str, $words = 20){
+		if($str){
+			$string = '';
+			$arr = explode(' ',$str);
+			if(count($arr) > $words){
+				for($i = 0; $i < $words; $i++){
+					$string .= ' '. $arr[$i];
+				}
+				return trim($string.'...');
+			}
+			return $str;
+		}
+	}
+
+	/**
+	 * @return array
+	 */
+	public static function getConfigDb(){
+		$dsn = str_replace('mysql:', '', Yii::$app->getDb()->dsn);
+		$dsn = explode(';', $dsn);
+		$db_user = Yii::$app->getDb()->username;
+		$db_pass = Yii::$app->getDb()->password;
+		$db_host = str_replace('host=', '', $dsn[0]);
+		$db_name = str_replace('dbname=', '', $dsn[1]);
+
+		return ['host' => $db_host, 'user' => $db_user, 'pass' => $db_pass, 'db_name' => $db_name];
+	}
+	/**
+	 * @param $host
+	 * @param $user
+	 * @param $pass
+	 * @param $name
+	 * @param string $tables
+	 */
+	public static function backupDB($host = null, $user = null, $pass = null, $name = null, $tables = '*')
+	{
+		if($host == null){
+			$host = Helper::getConfigDb()['host'];
+		}
+		if($user == null){
+			$user = Helper::getConfigDb()['user'];
+		}
+		if($pass == null){
+			$pass = Helper::getConfigDb()['pass'];
+		}
+		if($name == null){
+			$name = Helper::getConfigDb()['db_name'];
+		}
+
+		$path = Yii::$app->basePath.'/db/backups';
+		@chmod($path, 0777);
+
+		/* backup the db OR just a table */
+		$link = mysqli_connect($host, $user, $pass);
+		mysqli_select_db($link, $name);
+
+		//get all of the tables
+		if ($tables == '*') {
+			$tables = array();
+			$result = mysqli_query($link, 'SHOW TABLES');
+			while ($row = mysqli_fetch_row($result)) {
+				$tables[] = $row[0];
+			}
+		} else {
+			$tables = is_array($tables) ? $tables : explode(',', $tables);
+		}
+
+		$return = '';
+		//cycle through
+		foreach ($tables as $table) {
+			$result = mysqli_query($link, 'SELECT * FROM ' . $table);
+			$num_fields = mysqli_num_fields($result);
+
+			$return .= 'DROP TABLE ' . $table . ';';
+			$row2 = mysqli_fetch_row(mysqli_query($link, 'SHOW CREATE TABLE ' . $table));
+			$return .= "\n\n" . $row2[1] . ";\n\n";
+
+			for ($i = 0; $i < $num_fields; $i++) {
+				while ($row = mysqli_fetch_row($result)) {
+					$return .= 'INSERT INTO ' . $table . ' VALUES(';
+					for ($j = 0; $j < $num_fields; $j++) {
+						$row[$j] = addslashes($row[$j]);
+						$row[$j] = mb_ereg_replace("\n", "\\n", $row[$j]);
+						if (isset($row[$j])) {
+							$return .= '"' . $row[$j] . '"';
+						} else {
+							$return .= '""';
+						}
+						if ($j < ($num_fields - 1)) {
+							$return .= ',';
+						}
+					}
+					$return .= ");\n";
+				}
+			}
+			$return .= "\n\n\n";
+		}
+
+		//save file
+		$time = Common::currentUser() . '-' . Carbon::now(Common::currentUser('timezone'))->format(Datetime::FILE_TIME);
+		$handle = fopen($path.'/db-backup-' . $time . '.sql', 'w+');
+		fwrite($handle, $return);
+		fclose($handle);
+	}
 }
