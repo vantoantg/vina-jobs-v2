8bb856c006d7e77d9916c378a82c8a7e8239dc69
tona-lc <nvtoan@lockon-vn.com>
2018-03-01 17:34:24 +0700

    re gen model

--- models/JobCategories.php
+++ models/JobCategories.php
@@ -4,5 +4,37 @@ namespace app\models;
 
 class JobCategories extends \app\models\base\JobCategories
 {
+    /**
+     * @param bool $is_category
+     * @param int $parent_id
+     * @param string $space
+     * @param array $trees
+     * @return array
+     */
+    static public function categoriesMenus($is_category = true, $parent_id = 0, $space = "", $trees = [])
+    {
+        $table = self::tableName();
+        if (!$trees) {
+            $trees = [];
+        }
+        $sql = "SELECT * FROM $table WHERE (parent_id = {$parent_id}) order by arranged asc";
+        if (\Yii::$app->request->getQueryParam('id') && $is_category) {
+            $id = Yii::$app->request->getQueryParam('id');
+            if ($id > 0) {
+                $sql = "SELECT * FROM $table WHERE (parent_id = {$parent_id} AND id != {$id}) order by arranged asc";
+            }
+        }
+        $data = \Yii::$app->db->createCommand($sql)->queryAll();
+        foreach ($data as $k => $rs) {
+            $trees[] = array(
+                'id' => $rs['id'],
+                'name' => $space . $rs['name'],
+                'arranged' => $rs['arranged'],
+                'parent_id' => $rs['parent_id'],
+            );
+            $trees = self::categoriesMenus($is_category, $rs['id'], $space . ' - ', $trees);
+        }
 
+        return $trees;
+    }
 }
