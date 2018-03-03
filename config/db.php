1a190a563f7352280da944ef0b90e07c84f4f27f
Mr.Tona <nguyennguyen.vt88@gmail.com>
2018-03-04 00:02:09 +0700

    db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=vinajobs_db',
+    'dsn' => 'mysql:host=localhost;dbname=vina-jobs_vnj',
     'username' => 'vina-jobs_vnj',
     'password' => 'ROy2PzKot5',
     'charset' => 'utf8',
