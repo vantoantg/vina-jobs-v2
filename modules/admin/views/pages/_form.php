0aaa9edbf504b3f57d3e42e347151f215aa54e96
Mr.Tona <nguyennguyen.vt88@gmail.com>
2018-03-04 22:12:33 +0700

    pages

--- modules/admin/views/pages/_form.php
+++ modules/admin/views/pages/_form.php
@@ -16,7 +16,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true, 'readonly' => true]) ?>
 
 	<?= $form->field($model, 'content')->textarea(['rows' => 6, 'id' => 'page-content', 'class' => 'form-control']) ?>
 
