33b76d702f34a2dd083626385689b1932ed32480
tona-lc <nvtoan@lockon-vn.com>
2018-03-07 13:21:43 +0700

    SEO

--- config/web.php
+++ config/web.php
@@ -8,7 +8,7 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-	'language' => 'vi-VN',
+	'language' => 'vi_VN',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
