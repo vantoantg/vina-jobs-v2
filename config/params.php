d9d5e6e0ace485c34c9ce94d67dfa786111cdaf6
Mr.Tona <nguyennguyen.vt88@gmail.com>
2018-05-07 22:37:10 +0700

    SEO

--- config/params.php
+++ config/params.php
@@ -12,7 +12,7 @@ return [
     'seo' => [
         'title' => 'Tuyển dụng, tìm kiếm việc làm miễn phí tại jobs Vina nhanh chóng và hiệu quả',
         'description' => 'Tìm kiếm việc làm miễn phí - Tuyển dụng & Tìm kiếm việc làm nhanh, hiệu quả',
-        'keywords' => 'tuyển dụng, tuyen dung, tìm kiếm việc làm, tìm việc làm miễn phí, tim kiem viec lam mien phi, dang tin mien phi, sinh viên moi tot nghiep, đăng tin, dang tin tim viec, tìm việc làm lập trình web, lập trình php, kế toán, viec lam ban thi gian'
+        'keywords' => 'tuyển dụng, tuyen dung, tìm kiếm việc làm, tìm việc làm miễn phí, tim kiem viec lam mien phi, dang tin mien phi, tìm ứng viên miễn phí, tìm hồ sơ tuyển dụng miễn phím sinh viên moi tot nghiep, đăng tin, dang tin tim viec, tìm việc làm lập trình web, lập trình php, kế toán, viec lam ban thi gian'
     ],
 	'mail' => [
 		'host' => 'mail.jobsvina.com',
