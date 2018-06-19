26e73efe1ab3909f901b9600b2e858d7799dcaa3
Mr.Tona <nguyennguyen.vt88@gmail.com>
2018-06-19 22:35:59 +0700

    update log

--- config/private.php
+++ config/private.php
@@ -1,12 +1,33 @@
 <?php
+
 /*
- * Tona Nguyễn
+ *  Created by Tona Nguyen
+ *  Email: nguyennguyen.vt88@gmail.com
+ *  Phone: 0932.252.414
+ *  Address: VN, HCMC
+ *  Website: https://jobsvina.com/
  */
 
 return [
+    'encrypt' => [
+    	'my_simple_secret_key' => '',
+    	'my_simple_secret_iv' => '',
+    ],
+    'email' => [
+	    'host' => '',
+	    'username' => '',
+	    'password' => '',
+	    'secure' => true,
+	    'port' => '25',
+	    'options' => [
+		    'mailInfo' => 'info@jobsvina.com',
+		    'setFrom' => 'system@jobsvina.com',
+		    'addReplyTo' => 'noreply@jobsvina.com',
+	    ],
+    ],
     'db' => [
 	    'dsn' => 'mysql:host=localhost;dbname=zadmin_vnjobs',
         'username' => 'root',
         'password' => '123',
-    ]
+    ],
 ];
